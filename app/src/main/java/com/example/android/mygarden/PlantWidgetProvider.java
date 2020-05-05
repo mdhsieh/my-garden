426d497246ea45fdd88f6e4083bf87b161eec19b
Michael Hsieh <mdhsieh8@gmail.com>
2020-05-04 18:24:37 -0700

    feat: Create simple widget.
    Widget has an image that when clicked starts the main activity.

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -1,9 +1,13 @@
 package com.example.android.mygarden;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.example.android.mygarden.ui.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +17,20 @@ public class PlantWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        //CharSequence widgetText = context.getString(R.string.appwidget_text);
+
+        // Create an Intent to launch ExampleActivity
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
         // Construct the RemoteViews object
+        //RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget_provider);
+        //views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Get the layout for the App Widget and attach an on-click listener
+        // to the button
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setOnClickPendingIntent(R.id.widget_image, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
